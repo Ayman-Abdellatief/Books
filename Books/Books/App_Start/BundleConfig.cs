07c7fa665fd45e0483aa12da91ab2b11bd95d480
ayman.abdellatief <ayman.abdellatief@it-blocks.com>
2023-02-20 15:37:50 +0200

    Create View

--- Books/Books/App_Start/BundleConfig.cs
+++ Books/Books/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Books
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                        "~/Content/icofont.min.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Books/Books/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using Books.Models;
+using Books.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Books.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            var ViewModel = new BookFormViewModel()
+            {
+                categories = _context.categories.Where(x =>x.IsActive).ToList()
+        };
+
+            return View(ViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(BookFormViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                model.categories = _context.categories.Where(x => x.IsActive).ToList();
+                return View("Create",model);
+            }
+            var book = new Book
+            {
+                Title = model.Title,
+                Author = model.Author,
+                categoryId = model.categoryId,
+                Description = model.Description
+            };
+
+            _context.books.Add(book);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
